a79c777ce9b562bcff75b812287d37945f0c9a0b
Víctor Berga <vbergae@gmail.com>
2014-03-02 00:02:03 +0100

    Removes built-in support for user location
    - Adds the method 'GET:coordinates:handler' to perform requests based on
    coordinates.
    - Removes support for user location
    - Replaces default initializer from 'initWithOptions:' to
    'initWithBaseURL:options:
    - Extends documentation

--- Classes/ios/VODConnectionManager.h
+++ Classes/ios/VODConnectionManager.h
@@ -15,22 +15,41 @@
  */
 
 /**
- String used as the username's key
+ Default URL (http://mapas.valencia.es/lanzadera )
+ */
+extern NSString * const kVODBaseURLString;
+/**
+ String used as the username's key (username)
  */
 extern NSString * const kVODUsernameKey;
+/**
+ String used as the password's key (password)
+ */
 extern NSString * const kVODPasswordKey;
 
 /**
- VODConnectionManager
+ @name Helper functions
  */
-@interface VODConnectionManager : AFHTTPRequestOperationManager
-<CLLocationManagerDelegate>
 
 /**
- @name Properties
+ Generates a NSString instance from the latitude value
+ 
+ Transforms value to an integer number using the multiplier 10^6 and
+ generates a valid NSString instance from the resultant NSInteger.
+ 
+ @param value Reprensents a latitude or longitude value from 
+ CLLocationCoordinate2D struc
+ 
+ @return String without floting point values
  */
+NSString * NSStringFromCoordinateComponent(double value);
 
-@property (readonly) CLLocation *userLocation;
+/**
+ VODConnectionManager
+ 
+ Represents the connectio to a host and the interface to his actions.
+ */
+@interface VODConnectionManager : AFHTTPRequestOperationManager
 
 /**
  @name Creating a Connection Manager
@@ -44,17 +63,20 @@ extern NSString * const kVODPasswordKey;
  
  Options format:
  
-    @{
-        kVODUsernameKey : @"your_api_username",
-        kVODPasswordKey : @"password"
-    }
+ @{
+ kVODUsernameKey : @"your_api_username",
+ kVODPasswordKey : @"password"
+ }
  
  @warning Throws NSInternalInconsistencyException if options is nil or
-    username/password keys don't exists.
+ username/password keys don't exists.
  
- @param options
+ @param url The base URL for the HTTP client.
+ @param options The options dictionary with credentials
+ 
+ @return Initialized and configured connection manager
  */
-- (id)initWithOptions:(NSDictionary *)options;
+- (id)initWithBaseURL:(NSURL *)url options:(NSDictionary *)options;
 
 /**
  The default VODConnectionManager object for the host 
@@ -74,7 +96,32 @@ extern NSString * const kVODPasswordKey;
  @name Make request
  */
 
+/*+
+ Makes a request to URLFormat replacing coordinate's components templates
+ with the coordinates given.
+ 
+ Replaces ocurrences of strings {lat} coordinate's latitude and {lon} with 
+ longitude's coordinate.
+ 
+ @see GET:completion:
+ @param URLFormat End point with coordinate's template
+ @param coordinate Coordinates values
+ @param handler Completion handler
+ */
+- (void)GET:(NSString *)URLFormat
+coordinates:(CLLocationCoordinate2D)coordinate
+ completion:(void(^)(id response, NSError *error))handler;
+
+/*+
+ Makes a request to URLString.
+ 
+ Performs a GET request to URLString and callas handler block as callback.
+ 
+ @param URLFormat End point with coordinate's template
+ @param coordinate Coordinates values
+ @param handler Completion handler
+ */
 - (void)GET:(NSString *)URLString
- completion:(void(^)(id, NSError *))handler;
+completion:(void(^)(id, NSError *))handler;
 
 @end
--- Tests/VODConnectionManagerTests.m
+++ Tests/VODConnectionManagerTests.m
@@ -23,15 +23,48 @@
 {
     [super setUp];
 
+    NSURL *URL = [NSURL URLWithString:kVODBaseURLString];
     NSDictionary *options = @{kVODUsernameKey: @"username",
                               kVODPasswordKey : @"password"};
-    self.manager = [[VODConnectionManager alloc] initWithOptions:options];
+    self.manager = [[VODConnectionManager alloc] initWithBaseURL:URL
+                                                         options:options];
 }
 
 - (void)tearDown
 {
     self.manager = nil;
     [super tearDown];
+}
+
+#pragma mark -
+#pragma mark Helper functions
+
+- (void)test_NSStringFromCoordinateComponent
+{
+    unsigned char count = 6;
+    double values[]     = {
+        -23.9887,
+        23.9887,
+        0.000001,
+        32.876543,
+        -32.876543,
+        99.876543};
+    NSArray *expecteds = @[
+        @"-23988700",
+        @"23988700",
+        @"10000000",
+        @"32876543",
+        @"-32876543",
+        @"99876543"
+    ];
+    
+    for (NSUInteger i = 0; i < count; ++i) {
+        double value = values[i];
+        NSString *result = NSStringFromCoordinateComponent(value);
+        
+        XCTAssertEqualObjects(expecteds[i], result,
+                              @"should generate valid string");
+    }
 }
 
 #pragma mark -
@@ -52,16 +85,44 @@
 #pragma mark -
 #pragma mark Initialization
 
-- (void)test_initWithOptions_with_nil_options
+- (void)test_initWithBaseURL_options_with_nil_URL
 {
     XCTAssertThrows(
-        [[VODConnectionManager alloc] initWithOptions:nil],
+        [[VODConnectionManager alloc] initWithBaseURL:nil options:@{}],
+        @"should raise NSInternalInconsistencyException exception"
+    );
+}
+
+- (void)test_initWithBaseURL_options_with_nil_options
+{
+    NSURL *URL = [NSURL URLWithString:@"http://mapas.valencia.es"];
+    
+    XCTAssertThrows(
+        [[VODConnectionManager alloc] initWithBaseURL:URL   options:nil],
         @"should raise NSInternalInconsistencyException exception"
     );
 }
 
 #pragma mark -
 #pragma mark Instance methods
+
+#pragma mark GET:coordinates:compeltion:
+
+- (void)test_GET_coordinates_completion
+{
+    id manager = [OCMockObject partialMockForObject:self.manager];
+    [[manager expect] GET:@"path/-12345000/99876543" completion:OCMOCK_ANY];
+    
+    CLLocationCoordinate2D coordinates = CLLocationCoordinate2DMake(-12.345,
+                                                                    99.876543);
+    [manager GET:@"path/{lat}/{lon}"
+     coordinates:coordinates
+      completion:^(id response, NSError *error) {}];
+    
+    
+    XCTAssertNoThrow([manager verify],
+                     @"should call Get:completion:");
+}
 
 #pragma mark GET:completion:
 
@@ -83,42 +144,15 @@
 
 - (void)test_GET_completion
 {
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(
-        23.123456, -12.123456
-    );
-    id userLocation = [OCMockObject mockForClass:CLLocation.class];
-    [[[userLocation expect] andReturnValue:OCMOCK_VALUE(coordinate)] coordinate];
-    [[[userLocation expect] andReturnValue:OCMOCK_VALUE(coordinate)] coordinate];
-
     id manager = [OCMockObject partialMockForObject:self.manager];
-    [[[manager expect] andReturn:userLocation] userLocation];
-    [[[manager expect] andReturn:userLocation] userLocation];
     [[manager expect] GET:@"path/23123456/-12123456"
                parameters:OCMOCK_ANY
                   success:OCMOCK_ANY
                   failure:OCMOCK_ANY];
     
-    [manager GET:@"path/{lat}/{lon}" completion:^(id o, NSError *e) {}];
+    [manager GET:@"path/23123456/-12123456" completion:^(id o, NSError *e) {}];
     
-    XCTAssertNoThrow([userLocation verify], @"coordinates not called");
     XCTAssertNoThrow([manager verify], @"should make the request");
-}
-
-#pragma mark locationManager:didUpdateLocations:
-
-- (void)test_locationManager_didUpdaLocations
-{
-    NSArray *locations = @[CLLocation.new];
-    id manager = [OCMockObject mockForClass:CLLocationManager.class];
-    [[manager expect] stopUpdatingLocation];
-    
-    [self.manager locationManager:manager
-               didUpdateLocations:locations];
-    
-    XCTAssertNotNil(self.manager.userLocation,
-                    @"should set current user location");
-    XCTAssertNoThrow([manager verify],
-                     @"should stop user location");
 }
 
 #pragma mark -
